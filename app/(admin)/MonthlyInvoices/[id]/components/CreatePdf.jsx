3eb59e05f9e761c71aba99ab9298e3afbcfa1c0a
rafay <rafaykhan.mesh@gmail.com>
2024-09-23 23:17:07 +0500

    New Updates

--- app/(admin)/MonthlyInvoices/[id]/components/CreatePdf.jsx
+++ app/(admin)/MonthlyInvoices/[id]/components/CreatePdf.jsx
@@ -6,15 +6,24 @@ import { format, parse } from "date-fns"; // Make sure date-fns is imported
 import PdfButton from "./pdf/PdfButton";
 import { Loader } from "@mantine/core";
 
+function convertToISOString(dateString, hours = 11, minutes = 25, seconds = 4) {
+  const [day, month, year] = dateString.split("/");
+  const date = new Date(
+    Date.UTC(year, month - 1, day, hours, minutes, seconds)
+  );
+  return date.toISOString();
+}
+
+// Example usage
+
 export default function CreatePdf({ datesRange, user }) {
   const [loading, setLoading] = useState(true);
   const [myBookings, setMyBookings] = useState([]);
   const { allBookings } = useAdminContext();
-  console.log(myBookings);
 
   async function placeBookingsExistingAccsMonthly(email) {
-    const { start, end } = datesRange; 
-    
+    const { start, end } = datesRange;
+
     const startDate = parse(start, "dd/MM/yyyy", new Date());
     startDate.setHours(0, 0, 0, 0);
     const toDate = parse(end, "dd/MM/yyyy", new Date());
@@ -24,7 +33,8 @@ export default function CreatePdf({ datesRange, user }) {
 
     try {
       return allBookings.filter((booking) => {
-        const bookingDate = new Date(booking.createdAt.seconds * 1000);
+        const bookingDate = new Date(convertToISOString(booking.date));
+
         return (
           booking.userEmail.toLowerCase() === user.email.toLowerCase() &&
           bookingDate >= startDate &&
