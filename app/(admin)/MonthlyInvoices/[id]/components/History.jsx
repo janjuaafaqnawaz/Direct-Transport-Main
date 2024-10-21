cd6814d3be3e55aedc67d3e7ebb705c105b89a73
rafay <rafaykhan.mesh@gmail.com>
2024-10-22 00:20:33 +0500

    invoice email only to admin

--- app/(admin)/MonthlyInvoices/[id]/components/History.jsx
+++ app/(admin)/MonthlyInvoices/[id]/components/History.jsx
@@ -59,7 +59,8 @@ export default function History({ email }) {
 
     try {
       const templateParams = {
-        toEmail: cleanEmail,
+        toEmail: "bookings@courierssydney.com.au",
+        // cleanEmail
         url: url,
       };
 
