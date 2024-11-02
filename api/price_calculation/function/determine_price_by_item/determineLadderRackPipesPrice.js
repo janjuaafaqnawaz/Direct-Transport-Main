f54aeed58f197a98fdf8403863175886286c3f82
rafay <rafaykhan.mesh@gmail.com>
2024-11-02 05:18:16 +0500

    Feat: Pricing

--- api/price_calculation/function/determine_price_by_item/determineLadderRackPipesPrice.js
+++ api/price_calculation/function/determine_price_by_item/determineLadderRackPipesPrice.js
@@ -22,27 +22,26 @@ export default async function determineLadderRackPipesPrice(
     min_rate,
   });
 
-  if (distance >= 87 && total_weight >= 500) {
-    price = distance * 3.5;
-    returnType = "LD";
-    // console.log("satisfying", price, returnType);
-  } else {
-    if (total_weight < 100) {
-      if (longest_length <= 400) {
-        price = calculateBasePrice(distance, rate["HT"], min_rate["HT"]);
-        returnType = "HT";
-      } else {
-        price = calculateBasePrice(distance, rate["1T"], min_rate["1T"]);
-        returnType = "1T";
-      }
-    } else if (total_weight >= 100 && total_weight < 350) {
+  // if (distance >= 87 && total_weight >= 500) {
+  //   price = distance * 3.5;
+  //   returnType = "LD";
+  //   // console.log("satisfying", price, returnType);
+  // }
+  if (total_weight < 100) {
+    if (longest_length <= 400) {
+      price = calculateBasePrice(distance, rate["HT"], min_rate["HT"]);
+      returnType = "HT";
+    } else {
       price = calculateBasePrice(distance, rate["1T"], min_rate["1T"]);
       returnType = "1T";
-    } else {
-      const { cost, costType } = await TruckPricing(distance, items);
-      price = cost;
-      returnType = costType;
     }
+  } else if (total_weight >= 100 && total_weight < 350) {
+    price = calculateBasePrice(distance, rate["1T"], min_rate["1T"]);
+    returnType = "1T";
+  } else {
+    const { cost, costType } = await TruckPricing(distance, items);
+    price = cost;
+    returnType = costType;
   }
 
   return { price, returnType };
