0837cbfbea17ec2854242691998c96de8fee6a82
rafay <rafaykhan.mesh@gmail.com>
2024-11-06 06:49:26 +0500

    Feat: Optimize

--- api/price_calculation/function/helper/overCorrectSmallReturnType.js
+++ api/price_calculation/function/helper/overCorrectSmallReturnType.js
@@ -6,7 +6,7 @@ export default function overCorrectSmallReturnType(
     if (["Courier", "C", "HT", "1T"].includes(type) && longest_height > 200) {
       console.log("Longest height exceeds 2, changing job to '2T'");
       return "2T";
-    } else if (["Courier", "C"].includes(type) && longest_width > 100) {
+    } else if (["Courier", "C", "HT"].includes(type) && longest_width > 100) {
       console.log("Longest width exceeds 1, changing job to '1T'");
       return "1T";
     } else {
