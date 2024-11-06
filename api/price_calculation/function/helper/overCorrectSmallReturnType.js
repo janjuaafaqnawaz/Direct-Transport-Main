963eb07e75980b4e829d7a3c3c8c815e4162353a
Direct Transport Solutions <rajaafaqnawaz258@gmail.com>
2024-11-06 09:26:04 +0500

    Update overCorrectSmallReturnType.js

--- api/price_calculation/function/helper/overCorrectSmallReturnType.js
+++ api/price_calculation/function/helper/overCorrectSmallReturnType.js
@@ -1,7 +1,6 @@
 export default function overCorrectSmallReturnType(
   longest_height,
-  longest_width,
-  longest_length
+  longest_width
 ) {
   return function (type) {
     if (["Courier", "C", "HT", "1T"].includes(type) && longest_height > 200) {
@@ -10,7 +9,7 @@ export default function overCorrectSmallReturnType(
     } else if (
       ["Courier", "C", "HT"].includes(type) &&
       longest_width > 100 &&
-      longest_length > 100
+      longest_height > 100
     ) {
       console.log("Longest width exceeds 1, changing job to '1T'");
       return "1T";
